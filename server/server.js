9001cd023d2063c4eb610343ac628bf3db4b3785
Anushka Gaur <anushkagaur52@gmail.com>
2026-04-03 19:46:09 +0530

    server.js added

---
+++ server/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.listen(express.json());
+app.use(cors());
