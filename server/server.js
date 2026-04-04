14d58f96dcbf3beeaccd83fead8e289ef95a7a51
Anushka Gaur <anushkagaur52@gmail.com>
2026-04-04 22:22:53 +0530

    server.js

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
+import messageRoutes from "./routes/messageRoutes.js";
 
 dotenv.config();
 connectDB();
@@ -11,3 +12,4 @@ const app = express();
 
 app.listen(express.json());
 app.use(cors());
+app.use("/api/messages",messageRoutes);
