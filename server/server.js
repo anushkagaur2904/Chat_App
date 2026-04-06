1ce50249e26f9926b7cdb4992ba1cad7aee43874
Anushka Gaur <anushkagaur52@gmail.com>
2026-04-06 22:48:17 +0530

    server.js updated

--- server/server.js
+++ server/server.js
@@ -2,8 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
+import http from "http";
+import { setupSocket } from "./socket/socket.js";
 import authRoutes from "./routes/authRoutes.js";
 import messageRoutes from "./routes/messageRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+
 
 dotenv.config();
 connectDB();
@@ -13,3 +17,11 @@ const app = express();
 app.listen(express.json());
 app.use(cors());
 app.use("/api/messages",messageRoutes);
+app.use("/api/users", userRoutes);
+
+
+const server = http.createServer(app);
+
+setupSocket(server);
+
+server.listen(5000, () => console.log("Server running"));
